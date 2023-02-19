37be5364c22d4238bae73158ae53ec22042768d4
heartBig <748824306@qq.com>
2023-02-19 23:54:46 +0800

    todolist编写

--- App.tsx
+++ App.tsx
@@ -18,6 +18,7 @@ import {
   TextInput,
   Button,
   Alert,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -28,96 +29,74 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): JSX.Element {
-  // const [content] = useState('');
-  // const [msgList] = useState([]);
-  // const todoAdd = () => {
-  //   useState.centent = ''
-  // }
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? '#fff' : Colors.lighter,
+  };
+
+  const [content, setContent] = useState('');
+  const [todoList, setTodoList] = useState<Array<{content: string; finish:Boolean}>>([
+    {
+      content: "学习",
+      finish: false,
+    },
+    {
+      content: "吃饭",
+      finish: false,
+    },
+    {
+      content: "睡觉",
+      finish: false,
+    },
+  ]);
+
+  const Add = () => {
+    if (!content) return
+    const list = [...todoList]
+    list.unshift({
+      content: content,
+      finish: false,
+    })
+    setContent('')
+    setTodoList([...list])
+  }
+  const handleDel = (index: any) => {
+    let list = [...todoList]
+    list.splice(index, 1)
+    setTodoList([...list])
+  }
   return (
-    <View>
-      <View style={styles.addContainer}>
-        <TextInput style={styles.addInput} />
-        {/* <Button onPress={() => Alert.alert('Right button pressed')} style={styles.addButton}>Add</Button> */}
+    <SafeAreaView style={backgroundStyle}>
+      <View style={styles.addHeader}>
+        <TextInput placeholder='添加事项' value={content} style={styles.addInput} onChangeText={(e) => setContent(e)}/>
+        <Button title="Add" onPress={Add}></Button>
       </View>
-    </View>
+      <ScrollView style={styles.todoList}>
+        {todoList.map((item, index) => {
+          return (
+            <View key={index} style={styles.todoLi}>
+              <Text
+                style={styles.sectionTitle}>
+                {item.content}
+              </Text>
+              <TouchableOpacity onPress={() => handleDel(index)}>
+                <View style={styles.delButton}>
+                  <Text>X</Text>
+                </View> 
+              </TouchableOpacity>
+            </View>
+          )
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-
-// function App(): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step Oneffff 启动成功">
-//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// }
-
 const styles = StyleSheet.create({
-  addContainer: {
+  addHeader: {
     flexDirection: 'row',
     padding: 4,
   },
@@ -127,14 +106,34 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     marginRight: 6,
+    borderRadius: 4,
+    paddingLeft: 10,
   },
-  addButton: {
+  todoList: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 20,
+  },
+  todoLi: {
+    flexDirection: 'row',
+    fontSize: 24,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center', 
-    backgroundColor: 'steelblue',
-    color: 'white',
-    fontSize: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingLeft: 10,
+    marginBottom: 10,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  todoTitle: {
+    flex: 7,
+  },
+  delButton: {
+    width: 12,
+    height: 12,
+    flex: 1,
   },
   sectionContainer: {
     marginTop: 32,
